e75d07469727dcf49aab1ad057fb77de4d9094f3
mabaihua <baihua.ma@liveramp.com>
2019-11-08 17:57:52 +0800

    update config

--- src/test/java/com/ntu/datasync/DatasyncApplicationTests.java
+++ src/test/java/com/ntu/datasync/DatasyncApplicationTests.java
@@ -1,17 +1,20 @@
 package com.ntu.datasync;
 
+import com.ntu.datasync.config.MoquetteServer;
 import io.moquette.broker.Server;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
 @SpringBootTest
 class DatasyncApplicationTests {
-
+	@Autowired
+	private MoquetteServer moquetteServer;
 	@Test
 	void contextLoads() throws IOException {
-		Server.main(null);
+		moquetteServer.startServer();
 	}
 
 }
