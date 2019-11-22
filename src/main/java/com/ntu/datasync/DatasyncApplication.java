913c24151b1d4617f9dc23caa09de1426d6557a3
mabaihua <baihua.ma@liveramp.com>
2019-11-22 17:14:17 +0800

    s

--- src/main/java/com/ntu/datasync/DatasyncApplication.java
+++ src/main/java/com/ntu/datasync/DatasyncApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class DatasyncApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DatasyncApplication.class, args);
 	}
